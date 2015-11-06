76708285c08e8afebdaebe65d9e157bb2ac2a1a8
Thiago Chaves <thiagoarc@gmail.com>
2015-11-06 15:50:39 -0500

    Status dos contratos e aditivos pronto.

--- controller/contrato/aditivos.php
+++ controller/contrato/aditivos.php
@@ -8,7 +8,7 @@ $limit = $params->limit != '' ?  $params->limit : 10;
 
 try{
 
-	$sql = "SELECT a.idaditivo, a.idcontrato, a.numero, a.valor, DATE_FORMAT(a.validade, '%d/%m/%Y') as validade, a.obs, c.numerocontrato FROM aditivo a, contrato c WHERE a.idcontrato = c.idcontrato AND a.idcontrato = :idcontrato ORDER BY a.idaditivo asc LIMIT 0, $limit";
+	$sql = "SELECT a.idaditivo, a.idcontrato, a.numero, a.valor, DATE_FORMAT(a.validade, '%d/%m/%Y') as validade, a.obs, c.numerocontrato, TIMESTAMPDIFF(DAY, now(), a.validade) AS diasrestantes FROM aditivo a, contrato c WHERE a.idcontrato = c.idcontrato AND a.idcontrato = :idcontrato ORDER BY a.idaditivo asc LIMIT 0, $limit";
 	$stmt = $oConexao->prepare($sql);
 	$stmt->bindParam('idcontrato', $params->id);
 	$stmt->execute();  
