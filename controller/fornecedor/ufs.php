07de882e4bc5e079fd79111cdb3b5d7a6d08e4fa
Jaisson Santos <jaissonssantos@gmail.com>
2015-11-11 14:50:17 -0500

    Atualização no banco de dados.

--- controller/fornecedor/ufs.php
+++ controller/fornecedor/ufs.php
@@ -4,11 +4,9 @@ $oConexao = Conexao::getInstance();
 
 $params = json_decode(file_get_contents('php://input'));
 
-$limit = $params->limit != '' ?  $params->limit : 10;
-
 try{
 
-	$sql = "SELECT * FROM uf ORDER BY uf asc LIMIT 0, $limit";
+	$sql = "SELECT * FROM uf ORDER BY uf asc";
 	$stmt = $oConexao->query($sql);  
 	$cidades = $stmt->fetchAll(PDO::FETCH_OBJ);
 	$oConexao = null;
